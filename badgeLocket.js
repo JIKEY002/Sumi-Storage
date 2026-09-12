43e2faf387d8f38b1df0397c0460ff4c0c6895a5
NGUYỄN DUY KHÁNH <80080316+JIKEY002@users.noreply.github.com>
2026-09-13 01:53:56 +0700

    Add Content-Type header to API request options

--- badgeLocket.js
+++ badgeLocket.js
@@ -5,6 +5,7 @@ const options = {
     url: "https://api.locketcamera.com/changeProfileInfo",
     headers: {
         Authorization: request.headers["authorization"],
+        "Content-Type": "application/json",
         "User-Agent": request.headers["user-agent"]
     },
     body: JSON.stringify({
