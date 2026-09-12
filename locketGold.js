c3b909bc074a29b90431c26f9e6413d8b0298047
NGUYỄN DUY KHÁNH <80080316+JIKEY002@users.noreply.github.com>
2026-09-13 00:58:12 +0700

    Refactor locketGold.js for improved structure

--- locketGold.js
+++ locketGold.js
@@ -3,65 +3,68 @@ var request = $request;
 const options = {
     url: "https://api.revenuecat.com/v1/product_entitlement_mapping",
     headers: {
-     'Authorization' : request.headers["authorization"],
-     'X-Platform' : 'iOS' ,
-     'User-Agent' : request.headers["user-agent"]
+        Authorization: request.headers["authorization"],
+        "X-Platform": "iOS",
+        "User-Agent": request.headers["user-agent"]
     }
-}
+};
 
-$httpClient.get(options, function(error, newResponse, data){
-  
-const ent = JSON.parse(data);
+$httpClient.get(options, function (error, newResponse, data) {
+    const ent = JSON.parse(data);
 
-let jsonToUpdate = {
-        "request_date_ms": 1704070861000,
-        "request_date": "2006-04-03T01:01:01Z",
-        "subscriber": {
-            "entitlement": {},
-            "first_seen": "2006-04-03T01:01:01Z",
-            "original_application_version": "9692",
-            "last_seen": "2006-04-03T01:01:01Z",
-            "other_purchases": {},
-            "management_url": null,
-            "subscriptions": {},
-            "entitlements": {},
-            "original_purchase_date": "2006-04-03T01:01:01Z",
-            "original_app_user_id": "70B24288-83C4-4035-B001-573285B21AE2",
-            "non_subscriptions": {}
+    let jsonToUpdate = {
+        request_date_ms: 1704070861000,
+        request_date: "2006-04-03T01:01:01Z",
+        subscriber: {
+            non_subscriptions: {},
+            first_seen: "2006-04-03T01:01:01Z",
+            original_application_version: "2",
+            other_purchases: {},
+            management_url: "https://apps.apple.com/account/subscriptions",
+            subscriptions: {},
+            entitlements: {},
+            original_purchase_date: "2006-04-03T01:01:01Z",
+            original_app_user_id: "dK1MnzTyRZXeIPqjX4thAbFkfZE3",
+            last_seen: "2006-04-03T01:01:01Z"
         }
     };
 
-const productEntitlementMapping = ent.product_entitlement_mapping
+    const productEntitlementMapping = ent.product_entitlement_mapping;
 
-for (const [entitlementId, productInfo] of Object.entries(productEntitlementMapping)) {
-  const productIdentifier = productInfo.product_identifier;
-  const entitlements = productInfo.entitlements;
+    for (const [entitlementId, productInfo] of Object.entries(productEntitlementMapping)) {
+        const productIdentifier = productInfo.product_identifier;
+        const entitlements = productInfo.entitlements;
 
+        for (const entitlement of entitlements) {
+            console.log(`Entitlement: ${entitlement}`);
+            jsonToUpdate.subscriber.entitlements[entitlement] = {
+                grace_period_expires_date: null,
+                purchase_date: "2006-04-03T01:01:01Z",
+                product_identifier: productIdentifier,
+                expires_date: "9692-01-01T01:01:01Z"
+            };
 
-  for (const entitlement of entitlements) {
-    jsonToUpdate.subscriber.entitlements[entitlement] = {
-      "purchase_date": "2006-04-03T01:01:01Z",
-      "original_purchase_date": "2006-04-03T01:01:01Z",
-      "expires_date": "9692-01-01T01:01:01Z",
-      "is_sandbox" : false,
-      "ownership_type": "PURCHASED",
-      "store": "app_store",
-      "product_identifier": productIdentifier
-    };
+            // Add product identifier to subscriptions
+            jsonToUpdate.subscriber.subscriptions[productIdentifier] = {
+                auto_resume_date: null,
+                expires_date: "9692-01-01T01:01:01Z",
+                is_sandbox: false,
+                original_purchase_date: "2006-04-03T01:01:01Z",
+                refunded_at: null,
+                store_transaction_id: "580002295410467",
+                unsubscribe_detected_at: null,
+                grace_period_expires_date: null,
+                period_type: "normal",
+                purchase_date: "2006-04-03T01:01:01Z",
+                display_name: productIdentifier,
+                billing_issues_detected_at: null,
+                ownership_type: "PURCHASED",
+                store: "app_store",
+                management_url: "https://apps.apple.com/account/subscriptions"
+            };
+        }
+    }
 
-    // Add product identifier to subscriptions
-    jsonToUpdate.subscriber.subscriptions[productIdentifier] = {
-      "expires_date": "9692-01-01T01:01:01Z",
-      "original_purchase_date": "2006-04-03T01:01:01Z",
-      "purchase_date": "2006-04-03T01:01:01Z",
-      "is_sandbox" : false,
-      "ownership_type": "PURCHASED",
-      "store": "app_store"
-    };
-  }
-}
-
-body = JSON.stringify(jsonToUpdate);
-$done({body});
-
+    body = JSON.stringify(jsonToUpdate);
+    $done({ body });
 });
